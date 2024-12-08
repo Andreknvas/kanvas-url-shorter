93995da67ad3f00854f0d6f81ae53d7648c07a4a
Andreknvas <andreluiz1002@gmail.com>
2024-12-08 00:46:56 -0300

    init commit

---
+++ src/main/java/com/kanvas/urlshort/UrlData.java
@@ -0,0 +1,18 @@
+package com.kanvas.urlshort;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+
+public class UrlData {
+  private String originalUrl;
+
+  private long expirationTime;
+
+  
+
+}
